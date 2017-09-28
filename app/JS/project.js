948e28c5c9bdd84366228db05679629e26710945
Vladimir Maxymenko <462960@gmail.com>
2017-09-28 11:24:24 +0300

    Improved last version

--- app/JS/project.js
+++ app/JS/project.js
@@ -13,16 +13,15 @@ document.getElementById('search-box').focus();
 // Slide down menu
 $('#burger').click(function(e){
   e.preventDefault();
-  $('ul.drop-nav').toggleClass('slide-down');
-  $('ul.drop-nav').toggleClass('slide-up');
+  $('ul.drop-nav').toggleClass('slide-menu');
 });
+
 // Toggles background of slide down menu
 $('.drop-nav li').click(function(e){
   e.preventDefault();
   $('.drop-nav li').removeClass('active');
   $(this).addClass('active');
-  $('ul.drop-nav').removeClass('slide-down');
-  $('ul.drop-nav').addClass('slide-up');
+  $('ul.drop-nav').removeClass('slide-menu');
 });
 
 // Datepicker
